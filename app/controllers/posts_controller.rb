8b14367117f7755df90f6538e95947dbd20b5a20
David Mwangi <david.mathenge98@gmail.com>
2019-06-19 02:53:30 +0300

    feat(post): add create post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,30 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      redirect_to 'new'
+    end
+
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  # Security feature for rails that specifies what should be passed when creating a post
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
